68120ac61191fbf11442be6d00cb53f7f6b69ba5
fucar <fmucar@cooldatasoft.com>
2015-08-09 11:59:00 +0100

    Update to deploy script

--- deployAll.sh
+++ deployAll.sh
@@ -2,125 +2,38 @@
 
 WICKET_MENU_VERSION="2"
 
-## declare an array variable
-declare -a WICKET_VERSIONS=("6.9.0" "6.9.1" "6.10.0" "6.11.0" "6.12.0" "6.13.0" "6.14.0" "6.15.0" "6.16.0" "6.17.0" "6.18.0" "6.19.0" "6.20.0" "7.0.0")
+## declare versions of wicket that wicket-menu maintains
+#declare -a WICKET_VERSIONS=("6.9.0" "6.9.1" "6.10.0" "6.11.0" "6.12.0" "6.13.0" "6.14.0" "6.15.0" "6.16.0" "6.17.0" "6.18.0" "6.19.0" "6.20.0" "7.0.0")
+declare -a WICKET_VERSIONS=("6.9.0")
 
 ## now loop through the above array
 for wicketVersion in "${WICKET_VERSIONS[@]}"
 do
    echo "$wicketVersion"
-   #checkout branch for the specific version of wicket
+#   checkout branch for the specific version of wicket
    eval "git checkout  $wicketVersion"
-   #cahce username/password for git
+#   cahce username/password for git
    git config credential.helper store
    git fetch origin
    git pull
-   #update maven version number as the next release version
+#   update maven version number as the next release version
    eval "mvn versions:set -DnewVersion=$wicketVersion.$WICKET_MENU_VERSION"
-   #commit change to repo
+#   commit change to repo
    eval "git commit -m 'Release $wicketVersion.$WICKET_MENU_VERSION'"
    git push
-   #deploy to central repo
+
+#   deploy to central repo
 #   mvn clean deploy
-   #create release on github
-#   create relase tag
+
+
+    echo Creating release on github...
+    GITHUB_TOKEN=$(head -n 1 ~./m2/github-token)
+    curl -# -XPOST -H 'Content-Type:application/json' -H 'Accept:application/json' \
+    --data '{"tag_name": "$wicketVersion.$WICKET_MENU_VERSION", "target_commitish": "$wicketVersion", "name": "wicket-menu-$wicketVersion.$WICKET_MENU_VERSION", "body": "wicket-menu for using with wicket version $wicketVersion",  "draft": false,  "prerelease": false}' \
+    https://api.github.com/repos/cooldatasoft/wicket-menu/releases?access_token=$GITHUB_TOKEN -o response.json
+
 
 done
 
 git checkout master
-
-
-# You can access them using echo "${arr[0]}", "${arr[1]}" also
-
-
-#git checkout  6.9.0
-#mvn versions:set -DnewVersion=6.9.0.2
-#git commit -m "Release 6.9.0.2"
-#git push
-#mvn clean deploy
-#
-#
-#git checkout  6.9.1
-#mvn versions:set -DnewVersion=6.9.1.2
-#git commit -m "Release 6.9.1.2"
-#git push
-#mvn clean deploy
-#
-#
-#git checkout  6.10.0
-#mvn versions:set -DnewVersion=6.10.0.2
-#git commit -m "Release 6.10.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.11.0
-#mvn versions:set -DnewVersion=6.11.0.2
-#git commit -m "Release 6.11.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.12.0
-#mvn versions:set -DnewVersion=6.12.0.2
-#git commit -m "Release 6.12.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.13.0
-#mvn versions:set -DnewVersion=6.13.0.2
-#git commit -m "Release 6.13.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.14.0
-#mvn versions:set -DnewVersion=6.14.0.2
-#git commit -m "Release 6.14.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.15.0
-#mvn versions:set -DnewVersion=6.15.0.2
-#git commit -m "Release 6.15.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.16.0
-#mvn versions:set -DnewVersion=6.16.0.2
-#git commit -m "Release 6.16.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.17.0
-#mvn versions:set -DnewVersion=6.17.0.2
-#git commit -m "Release 6.17.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.18.0
-#mvn versions:set -DnewVersion=6.18.0.2
-#git commit -m "Release 6.18.0.2"
-#git push
-#mvn clean deploy
-#
-#git checkout  6.19.0
-#mvn versions:set -DnewVersion=6.19.0.2
-#git commit -m "Release 6.19.0.2"
-#git push
-#mvn clean deploy
-#
-#
-#git checkout  6.20.0
-#mvn versions:set -DnewVersion=6.20.0.2
-#git commit -m "Release 6.20.0.2"
-#git push
-#mvn clean deploy
-#
-#
-#git checkout  7.0.0
-#mvn versions:set -DnewVersion=7.0.0.2
-#git commit -m "Release 7.0.0.2"
-#git push
-#mvn clean deploy
-
-
-
 
