98e6eea334dfb4f9375ac23ea15a243f54ee6595
fucar <fmucar@cooldatasoft.com>
2015-08-09 12:56:15 +0100

    updating deploy script

--- deployAll.sh
+++ deployAll.sh
@@ -4,7 +4,6 @@ WICKET_MENU_VERSION="2"
 
 ## declare versions of wicket that wicket-menu maintains
 declare -a WICKET_VERSIONS=("6.9.0" "6.9.1" "6.10.0" "6.11.0" "6.12.0" "6.13.0" "6.14.0" "6.15.0" "6.16.0" "6.17.0" "6.18.0" "6.19.0" "6.20.0" "7.0.0")
-declare -a WICKET_VERSIONS=("6.12.0" "6.14.0" "6.17.0" "6.19.0")
 #declare -a WICKET_VERSIONS=("6.9.0")
 
 ## now loop through the above array
@@ -23,14 +22,12 @@ do
 
    eval "git commit -m 'Release $wicketVersion.$WICKET_MENU_VERSION'"
    git push
-   wait 2000
 
-#   mvn clean deploy
+   mvn clean deploy
 
    echo Creating release on github...
    GITHUB_TOKEN=$(head -n 1 ~/.m2/github-token)
    eval "curl -# -XPOST -H 'Content-Type:application/json' -H 'Accept:application/json' -d  '{\"tag_name\": \"$wicketVersion.$WICKET_MENU_VERSION\", \"target_commitish\": \"$wicketVersion\", \"name\": \"wicket-menu-$wicketVersion.$WICKET_MENU_VERSION\", \"body\": \"wicket-menu for using with wicket version $wicketVersion\",  \"draft\": false,  \"prerelease\": false}'  https://api.github.com/repos/cooldatasoft/wicket-menu/releases?access_token=$GITHUB_TOKEN"
-   wait 5000
 done
 
 
