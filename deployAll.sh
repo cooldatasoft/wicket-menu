ab90ac4638d393b5c43c47fcce53b6e963fd9096
fucar <fmucar@cooldatasoft.com>
2015-08-12 18:09:09 +0100

    updating deploy script

--- deployAll.sh
+++ deployAll.sh
@@ -4,7 +4,7 @@ pwd
 WICKET_MENU_VERSION="3"
 ## declare versions of wicket that wicket-menu maintains
 #declare -a WICKET_VERSIONS=("6.9.0" "6.9.1" "6.10.0" "6.11.0" "6.12.0" "6.13.0" "6.14.0" "6.15.0" "6.16.0" "6.17.0" "6.18.0" "6.19.0" "6.20.0" "7.0.0")
-declare -a WICKET_VERSIONS=("6.9.0")
+declare -a WICKET_VERSIONS=("6.9.1")
 
 ## now loop through the above array
 for wicketVersion in "${WICKET_VERSIONS[@]}"
@@ -17,18 +17,10 @@ do
    git fetch origin
    git pull
 
-#   git cherry-pick ce8d8bd545b90f3e59c60a8ee417720308e77a1d
-#   git cherry-pick 2de128dd0f0e64e5e7cacb153b8d52ad2d2d6700
-#   git cherry-pick 6c822c7e67389d838e75252fc3e6875f469e85ab
-#   git cherry-pick 05a2699aff4926a8f332338c65bdd35baa97e76f
-#   git cherry-pick f6c5b69bc71d1dc0407181c29640c46daeaef2bf
-#   git cherry-pick 9c98e9ca1e17ccfe55f21b355d9e399fefdc8c98
-#   git cherry-pick 8afc8235870884c252cb2dc2697fac35d916bba0
-#   git cherry-pick 3a59404392a982eef0e5b904cc348e1dd3d1f2e6
-#   git cherry-pick 3a296688361c3f13cdefe8a709ec6a527377360b
-#   git cherry-pick efa2ec2a918217abdf8a7e44fc844c829312b4d5
 
+   git cherry-pick c5eeee99353cd370b0393d32d9f8f45b344c611a
    git cherry-pick a3571e8845532cdfad98ee71ea0602383337b638
+   exit
 
    eval "mvn versions:set -DnewVersion=$wicketVersion.$WICKET_MENU_VERSION -DgenerateBackupPoms=false"
 
